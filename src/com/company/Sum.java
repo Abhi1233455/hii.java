ce422a52fe4482f3a1c288d2832a98f7146316cb
Abhishek <abhishekgond634@gmail.com>
2022-02-07 19:12:32 +0530

    Commit Sum

---
+++ src/com/company/Sum.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Sum {
+    public static void main(String[] args) {
+
+
+        int a = 5;
+        int b = 54;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
